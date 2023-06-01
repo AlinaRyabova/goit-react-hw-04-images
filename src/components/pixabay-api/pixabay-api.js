0875b0ab1b27d663c2ceff20cfaef0a60e67e3f9
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-06-01 13:16:03 +0300

    update jsx

---
+++ src/components/pixabay-api/pixabay-api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const API_KEY = '35498849-b2ea51a452a2f38037ac49000';
+
+export const perPage = 12;
+
+export const getImages = async (query, page) => {
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response.data;
+};
+
+export const normalizedImages = imagesArray =>
+  imagesArray.map(({ id, tags, webformatURL, largeImageURL }) => {
+    return { id, tags, webformatURL, largeImageURL };
+  });
